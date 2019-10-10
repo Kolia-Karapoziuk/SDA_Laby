b321dbcf6d3a78d1b8f730302fe368d8f3af1f11
Kolian_Karapoziuk <54900149+Kolia-Karapoziuk@users.noreply.github.com>
2019-10-10 22:11:33 +0300

    Update Source.cpp

--- SDA_Karapoziuk_L3/SDA_Karapoziuk_L3/Source.cpp
+++ SDA_Karapoziuk_L3/SDA_Karapoziuk_L3/Source.cpp
@@ -1,43 +1,53 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 int counter = 0;
+int counterMethod = 0;
+int counMet = 0;
 
 int recursiveMethod(char* str, int count)
 {
+	counMet++;
 	if (isdigit(*str))
 	{
 		count++;
 	}
-
 	if (*str == '\0')
 	{
 		return count;
 	}
 
-	recursiveMethod(++str, count);
+	recursiveMethod(++str, count);	
 }
 
 void iterativeMethod(string str)
 {
-	for (size_t i = 0; i != str.size(); i++)
+	/*for (size_t i = 0; i != str.size(); i++)
 	{
 		if (isdigit(str[i]))
 		{
 			counter++;
 		}
+		counterMethod++;
 	}
-	
+	*/
+
+	cout << count_if(str.begin(), str.end(), isdigit) << endl;
 }
 
 int main() {
+	
 	char S[100] = "BmW 476dc55625255 London";
+	
 	cout << "Number of digits(Recursive method): " << recursiveMethod(S, 0) << endl;
+	cout<< "Number of repetitions: " << counMet << endl << endl;
 
+	
 	string str("BmW 476dc55625255 London");
 	iterativeMethod(str);
-	cout << "Number of digits(Iterative method): " << counter << endl;
+	cout << "Number of digits(Iterative method): " << counter << endl << "Number of repetitions: " << counterMethod << endl;
 
 	return 0;
 }
